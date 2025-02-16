e8759a0a1c6e95fe4a70591090f8b90a737e9ea6
deepkoley156 <deepkoley156@gmail.com>
2025-02-17 03:03:10 +0530

    app

--- app.py
+++ app.py
@@ -1,14 +1,14 @@
-from flask import Flask, jsonify, render_template
+from flask import Flask, jsonify
 import requests
 
 app = Flask(__name__)
 
 @app.route('/')
 def home():
-    api_url = "https://min-api.cryptocompare.com/data/price?fsym=BTC&tsyms=USD,JPY,EUR"
-    response = requests.get(api_url)
+    url = "https://min-api.cryptocompare.com/data/price?fsym=BTC&tsyms=USD,JPY,EUR"
+    response = requests.get(url)
     data = response.json()
-    return render_template('index.html', price=data)
+    return f"<h1>BTC Price</h1><p>USD: {data['USD']}$</p><p>JPY: {data['JPY']}¥</p><p>EUR: {data['EUR']}€</p>"
 
-if __name__ == "__main__":
+if __name__ == '__main__':
     app.run(debug=True)
