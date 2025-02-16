b18427b6834e9ce6df5d99a9453a5e1f4536a830
deepkoley156 <deepkoley156@gmail.com>
2025-02-17 02:54:22 +0530

    app.py

--- app.py
+++ app.py
@@ -1,10 +1,14 @@
-from flask import Flask
+from flask import Flask, jsonify, render_template
+import requests
 
 app = Flask(__name__)
 
 @app.route('/')
 def home():
-    return "Hello, Crypto Signal Bot is Running!"
+    api_url = "https://min-api.cryptocompare.com/data/price?fsym=BTC&tsyms=USD,JPY,EUR"
+    response = requests.get(api_url)
+    data = response.json()
+    return render_template('index.html', price=data)
 
 if __name__ == "__main__":
-    app.run(host='0.0.0.0', port=5000)
+    app.run(debug=True)
